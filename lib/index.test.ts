a104250b71d8aef5abb8d5b9b40ff8f673fb897c
pillar <ymblender@gmail.com>
2022-07-02 04:56:42 +0800

    fix: add test

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -11,12 +11,21 @@ describe("simple error", () => {
     expect(err.message).toMatch(/error in promise/);
   });
 
-  it("simple promies obj", async () => {
+  it("simple promies not", async () => {
     const errorInPromise = async () => {
       throw Error("error in promise");
     };
     const err = await errorGet(errorInPromise());
 
     expect(err).toMatch(/error in promise/);
+  });
+
+  it("simple promies not error", async () => {
+    const errorInPromise = async () => {
+      return { dog: "aaa", age: 5 };
+    };
+    const err = await errorGet(errorInPromise());
+
+    expect(err).deep.eq({ dog: "aaa", age: 5 });
   });
 });
